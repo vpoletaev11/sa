eb896bf3e55d0f80e838e479cd8ca1077acc51cd
vpoletaev11 <vpoletaev11@gmail.com>
2019-03-13 22:20:34 +0300

    adapterStruct in developing

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 )
 
+//contain parameters for adapter
 type adapter struct {
 	number int
 	name   string
@@ -18,9 +19,11 @@ func main() {
 		log.Fatal(err)
 	}
 	check := prettyOut(ipOutputByte)
-	fmt.Println(check)
+	spaces := adapterSrtuct(check)
+	fmt.Println(check, spaces)
 }
 
+//return one string for one adapter from ip link
 func prettyOut(out []byte) []string {
 	//massive with strings for every adapter
 	var strings []string
@@ -33,7 +36,7 @@ func prettyOut(out []byte) []string {
 			lastN = i
 		}
 	}
-	//split strings for every single adapter
+	//join strings for every single adapter
 	var adapterStr []string
 	for i := 0; i < len(strings); i += 2 {
 		if i == 0 {
@@ -42,4 +45,22 @@ func prettyOut(out []byte) []string {
 		adapterStr = append(adapterStr, strings[i-1]+strings[i])
 	}
 	return adapterStr
+}
+
+//make slice of adapters from slice of strings
+func adapterSrtuct(s []string) [][]int {
+	var spaces [][]int
+	for i := 0; i < len(s); i++ {
+		currentStr := string(s[i])
+		var currentStrSpaces []int
+		for a := 0; a < len(currentStr); a++ {
+			if string(currentStr[a]) == " " {
+				currentStrSpaces = append(currentStrSpaces, a+1)
+			}
+			if a == len(currentStr)-1 {
+				spaces = append(spaces, currentStrSpaces)
+			}
+		}
+	}
+	return spaces
 }
