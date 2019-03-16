48e3ce9db1cb806de7e7115748c850f4e55a8c37
vpoletaev11 <vpoletaev11@gmail.com>
2019-03-16 21:32:46 +0300

    Func wordExtractor done

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ type adapter struct {
 	number int
 	name   string
 	mac    string
+	mode   string
 }
 
 func main() {
@@ -19,8 +20,8 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	check := cutter(ipOutputByte)
-	fmt.Println(check)
+	cutted := cutter(ipOutputByte)
+	fmt.Println(wordExtractor(cutted[0], 0))
 }
 
 // cut ip link output to strings
@@ -28,4 +29,36 @@ func cutter(text []byte) []string {
 	clean := strings.Replace(string(text), "\n    ", " ", -1)
 	clean = clean[:len(clean)-1]
 	return strings.Split(clean, "\n")
+}
+
+// func agregator(value []string) []adapter {
+// }
+
+// extract word by position
+func wordExtractor(value string, position int) string {
+	// create slice of spaces for value
+	spaces := []int{}
+	for i := range value {
+		if string(value[i]) == " " {
+			spaces = append(spaces, i)
+		}
+	}
+	// create slice of words for value
+	words := []string{}
+	prevSpace := 0
+	for _, index := range spaces {
+		word := ""
+		if position == 0 {
+			word = value[:index]
+		} else {
+			word = value[prevSpace+1 : index]
+		}
+		if string(word[len(word)-1]) == ":" {
+			words = append(words, word[:len(word)-1])
+		} else {
+			words = append(words, word)
+		}
+		prevSpace = index
+	}
+	return words[position]
 }
--- main_test.go
+++ main_test.go
@@ -14,13 +14,49 @@ const ipLinkOut = `1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state U
     link/ether e0:b9:a5:1e:e0:88 brd ff:ff:ff:ff:ff:ff
 `
 
-func TestPrettyOut(t *testing.T) {
+func TestCutter(t *testing.T) {
 	//arrange
-	expectedPrettyOut := []string{
+	expected := []string{
 		"1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000 link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", "2: enp1s0: <NO-CARRIER,BROADCAST,MULTICAST,UP> mtu 1500 qdisc mq state DOWN mode DEFAULT group default qlen 1000 link/ether 00:8c:fa:6a:09:87 brd ff:ff:ff:ff:ff:ff", "3: wlp2s0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP mode DORMANT group default qlen 1000 link/ether e0:b9:a5:1e:e0:88 brd ff:ff:ff:ff:ff:ff",
 	}
 	//act
 	result := cutter([]byte(ipLinkOut))
 	//assert
-	assert.Equal(t, expectedPrettyOut, result)
+	assert.Equal(t, expected, result)
+}
+
+func TestWordExtractor01(t *testing.T) {
+	//arrange
+	expected := "1"
+	//act
+	result := wordExtractor("1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000 link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", 0)
+	//assert
+	assert.Equal(t, expected, result)
+}
+
+func TestWordExtractor02(t *testing.T) {
+	//arrange
+	expected := "lo"
+	//act
+	result := wordExtractor("1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000 link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", 1)
+	//assert
+	assert.Equal(t, expected, result)
+}
+
+func TestWordExtractor03(t *testing.T) {
+	//arrange
+	expected := "00:00:00:00:00:00"
+	//act
+	result := wordExtractor("1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000 link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", 16)
+	//assert
+	assert.Equal(t, expected, result)
+}
+
+func TestWordExtractor04(t *testing.T) {
+	//arrange
+	expected := "DEFAULT"
+	//act
+	result := wordExtractor("1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000 link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00", 10)
+	//assert
+	assert.Equal(t, expected, result)
 }
